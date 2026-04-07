a3135c3154292d3d381d966852517f946a670f45
Ezana577 <ezana071@gmail.com>
2026-04-07 13:03:03 -0400

    Create constants.js

---
+++ config/constants.js
@@ -0,0 +1,7 @@
+export const EMBED_COLOR = 0x1F2A44;
+
+export const PREFIX = process.env.PREFIX || '<';
+
+export const PING_COOLDOWN_MS = 10000;
+
+export const BOT_NAME = 'PRPC Department of Justice';
