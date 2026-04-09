6ce78cc747c29bec7807d9d0323765008ad04e4d
Ezana577 <ezana071@gmail.com>
2026-04-08 23:48:52 -0400

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,27 +1,30 @@
-import { Events, ActivityType, REST, Routes } from 'discord.js';
-import { logger } from '../utils/logger.js';
+import { Events, ActivityType, REST, Routes } from ‘discord.js’;
+import { logger } from ‘../utils/logger.js’;
+import { startSyncInterval } from ‘../utils/rolesync.js’;
 
 export const name = Events.ClientReady;
 export const once = true;
 
 export async function execute(client) {
-  logger.info('READY', `Logged in as ${client.user.tag}`);
-  logger.info('READY', `Active in ${client.guilds.cache.size} guild(s)`);
+logger.info(‘READY’, `Logged in as ${client.user.tag}`);
+logger.info(‘READY’, `Active in ${client.guilds.cache.size} guild(s)`);
 
-  client.user.setPresence({
-    activities: [{ name: 'Department of Justice', type: ActivityType.Watching }],
-    status: 'online',
-  });
+client.user.setPresence({
+activities: [{ name: ‘Department of Justice’, type: ActivityType.Watching }],
+status: ‘online’,
+});
 
-  try {
-    const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
-    logger.info('COMMANDS', `Registering ${client.commandPayloads.length} slash command(s)...`);
-    await rest.put(
-      Routes.applicationCommands(process.env.CLIENT_ID),
-      { body: client.commandPayloads }
-    );
-    logger.info('COMMANDS', 'Slash commands registered successfully.');
-  } catch (err) {
-    logger.error('COMMANDS', 'Failed to register slash commands', err);
-  }
+try {
+const rest = new REST({ version: ‘10’ }).setToken(process.env.DISCORD_TOKEN);
+logger.info(‘COMMANDS’, `Registering ${client.commandPayloads.length} slash command(s)...`);
+await rest.put(
+Routes.applicationCommands(process.env.CLIENT_ID),
+{ body: client.commandPayloads }
+);
+logger.info(‘COMMANDS’, ‘Slash commands registered successfully.’);
+} catch (err) {
+logger.error(‘COMMANDS’, ‘Failed to register slash commands’, err);
+}
+
+startSyncInterval(client);
 }
