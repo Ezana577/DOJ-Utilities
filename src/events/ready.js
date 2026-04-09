600c4d0e110cbad09e2a0e62ee5f3882481f4efb
Ezana577 <ezana071@gmail.com>
2026-04-09 11:42:26 -0400

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,15 @@
-await rest.put(
-  Routes.applicationCommands(process.env.CLIENT_ID),
-  { body: [] }
-);
+import { Events, REST, Routes } from 'discord.js';
+
+export const name = Events.ClientReady;
+export const once = true;
+
+export async function execute(client) {
+  const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+  await rest.put(
+    Routes.applicationCommands(process.env.CLIENT_ID),
+    { body: [] } // wipe all commands
+  );
+
+  console.log('All commands wiped.');
+}
